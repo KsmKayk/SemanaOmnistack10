091bfdea9e896de78aa8f3b0f459702d30bb704e
Kayk <ksm.kayk@outlook.com>
2020-01-15 20:02:04 -0300

    FrontEnd

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes.js");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,7 +14,7 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,16 @@
 // arquivo de rotas... Chama o express e os controllers
 
 const { Router } = require("express");
+const AdmController = require("./controllers/ADMController");
 const DevController = require("./controllers/DevController");
 const SearchController = require("./controllers/SearchController");
 
 const routes = Router();
 
 // cria a rota dev e a search com suas funções
+routes.post("/adms", AdmController.store);
+routes.get("/adms", AdmController.index);
+
 routes.post("/devs", DevController.store); // cadastrar
 routes.get("/devs", DevController.index); // visualizar
 routes.put("/devs", DevController.update); // editar
